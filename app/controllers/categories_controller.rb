9ce77315b1fa436903d3132ed07508861b7587d0
LRosdobutko <lrosdobutko@academic.rrc.ca>
2024-11-14 19:01:33 -0600

    added error handling to Category view

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -17,7 +17,7 @@ class CategoriesController < ApplicationController
     if @category.save
       redirect_to @category
     else
-      render: :new, status: :unprocessable_entity
+      render :new, status: :unprocessable_entity
     end
 
   end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,5 @@
 class Category < ApplicationRecord
+
+  validates :name, presence: true
+  validates :description, presence: true, length: {maximum: 25}
 end
