c3763bbbdef011e9be45a91296600d084019a2e4
LRosdobutko <lrosdobutko@academic.rrc.ca>
2024-11-27 13:28:15 -0600

    added search functions

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,6 +8,8 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+  before_action :set_book, only: [:edit, :update, :show]
+
   def new
     @book = Book.new
   end
@@ -16,30 +18,56 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
 
     if @book.save
-      redirect_to @book
+      redirect_to books_path, notice: 'Book was successfully created.'
     else
-      render :new, status: :unprocessable_entity
+      render :new
     end
   end
+
 
   def edit
     @book = Book.find(params[:id])
   end
 
   def update
-    @book = Book.find(params[:id])
-
     if @book.update(book_params)
-      redirect_to @book
+      redirect_to @book, notice: 'Book was successfully updated.'
     else
-      render :edit, status: :unprocessable_entity
+      render :edit
     end
   end
 
+  def search
+    @books = Book.all
+
+    if params[:search_type].present? && params[:query].present?
+      case params[:search_type]
+      when 'book_name'
+        @books = @books.where("title LIKE ?", "%#{params[:query]}%")
+      when 'genre'
+        @books = @books.joins(:genre).where("genres.name LIKE ?", "%#{params[:query]}%")
+      end
+    end
+
+    if params[:on_sale] == '1'  # '1' is sent if checkbox is checked
+      @books = @books.where(on_sale: true)
+    end
+
+    if params[:recently_added] == '1'  # '1' is sent if checkbox is checked
+      @books = @books.where("created_at >= ?", 3.days.ago)
+    end
+
+    render :search_results
+  end
 
   private
+
+  def set_book
+    @book = Book.find(params[:id])
+  end
+
   def book_params
-    params.require(:book).permit(:title, :author, :publisher, :synopsis, :price_cents, :Genre_id, :on_sale)
+    params.require(:book).permit(:title, :author, :synopsis, :price_cents, :genre_id, :on_sale)
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+
+  root "books#index" # or replace with your desired controller#action
+
   resources :genres do
     resources :books
   end
-  resources :books
+
+  resources :books do
+    collection do
+      get :search
+    end
+  end
+
 end
