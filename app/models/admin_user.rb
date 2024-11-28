d4c36b8996c9aebfa04b5b480c153b27f2444878
LRosdobutko <lrosdobutko@academic.rrc.ca>
2024-11-28 08:20:08 -0600

    admin dashboard has correct permissions enabled

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,6 +1,12 @@
 class AdminUser < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, 
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["email", "created_at", "updated_at"] # Include only the attributes you want searchable
+  end
+
+  devise :database_authenticatable,
          :recoverable, :rememberable, :validatable
+
 end
