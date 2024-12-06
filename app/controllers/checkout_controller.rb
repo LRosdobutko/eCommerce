f33121cc3a3b93b3108e852f9789e012806de325
LRosdobutko <lrosdobutko@academic.rrc.ca>
2024-12-06 12:08:47 -0600

    added checkout functionality. redirect does not occur. CORS issue.

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -3,6 +3,31 @@ class CheckoutController < ApplicationController
   def create
     book = Book.find(params[:book_id])
 
+    if book.nil?
+      redirect_to root_path
+      return
+    end
+
+    @session = Stripe::Checkout::Session.create(
+      payment_method_types: ["card"],
+      success_url: checkout_success_url,
+      cancel_url: checkout_cancel_url,
+      mode: "payment",
+      line_items: [
+        price_data: {
+          currency: "cad",
+          product_data: {
+            name: book.title,
+            description: book.synopsis,
+          },
+          unit_amount: book.price_cents,
+        },
+        quantity: 1
+      ]
+    )
+
+    redirect_to @session.url, allow_other_host: true
+
 
 
 
